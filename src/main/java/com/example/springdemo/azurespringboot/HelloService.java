b686c974948a706e6b0fdd3e8a86868229935335
lmencnar <lmencnar@gmail.com>
2023-04-08 21:29:43 +0200

    switched to springdoc, list environment to log

--- src/main/java/com/example/springdemo/azurespringboot/HelloService.java
+++ src/main/java/com/example/springdemo/azurespringboot/HelloService.java
@@ -2,10 +2,17 @@ package com.example.springdemo.azurespringboot;
 
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class HelloService {
 
     public String getMessage(String name) {
+
+        System.getenv().entrySet().stream().forEach(action -> {
+            log.info("env " + action.getKey() + "=" + action.getValue());
+        });
         return "Hello " + name + "!";
     }
     
